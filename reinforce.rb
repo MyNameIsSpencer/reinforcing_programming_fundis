bf2d3c37ba263642361d0ce4d47fe20a388ffc67
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-06 19:07:23 -0500

    reinforcement complete

---
+++ reinforce.rb
@@ -0,0 +1,60 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  due_date: "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     due_date: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     due_date: "January 1, 2018"
+    },
+    {description: "compile results",
+     due_date: "November 10, 2018"
+    },
+    {description: "version 1",
+     due_date: "January 15, 2019"
+    },
+    {description: "revisions",
+     due_date: "March 30, 2019"
+    },
+    {description: "version 2",
+     due_date: "July 12, 2019"
+    },
+    {description: "final edit",
+     due_date: "October 1, 2019"
+    },
+    {description: "final version",
+     due_date: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     due_date: "December 1, 2019"
+    }
+  ]
+}
+
+# if counter = 2
+
+big_counter = 0
+small_counter = 0
+
+project[:steps].each do |hasher|
+  hasher[:person] = project[:committee][big_counter]
+  small_counter += 1
+  if small_counter == 3
+    big_counter += 1
+    small_counter = 0
+  end
+end
+
+
+puts '-' * 30
+puts '-' * 30
+puts '-' * 30
+
+puts project
+
+puts '-' * 30
+puts '-' * 30
+puts '-' * 30
